b217a887c292894fda7870e84389038dd53b1ba9
babtor <lucaswikstrom96@gmail.com>
2023-05-13 10:19:59 +0200

    attempted to write async function to fetch weather data based on location, not working properly.

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-fetch('https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid={16e5867aeca8a9ab08e5612a4596aed5}');
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=Stockholm,SWE&appid=16e5867aeca8a9ab08e5612a4596aed5')
-
-
-
 //Asks the user for their current location, returns longitude and latitude values
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-function successCallback(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    console.log(latitude);
-    console.log(longitude);
-}
-function errorCallback(error) {
-    console.error('Error occurred while retrieving location:', error);
-  }
+async function fetchData(latitude, longitude) {
+    const apiKey = '16e5867aeca8a9ab08e5612a4596aed5';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}';
+
+    try {
+        const response = await fetch(apiUrl);
+        if (response.ok) {
+          const data = await response.json();
+          // Handle the response data from OpenWeatherMap API
+          console.log(data);
+          // Extract and use the desired weather information from the response
+        } else {
+          throw new Error('Error occurred while fetching data');
+        }
+      } catch (error) {
+        // Handle any errors that occurred during the API request
+        console.error('Error:', error);
+      }
+    }
+    
+    function successCallback(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+    
+      fetchData(latitude, longitude);
+    }
+    
+    function errorCallback(error) {
+      console.error('Error occurred while retrieving location:', error);
+    }
+    
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
