81f215644d60c179bcceaa2e382fe13120d4ff60
babtor <lucaswikstrom96@gmail.com>
2023-05-13 17:33:45 +0200

    Displayed location data from fetch call

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let apiKey = 'e79a4558d37f0ddb584eb4a4854fb14b';
 
+
 function successCallback(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
@@ -10,10 +11,20 @@ function successCallback(position) {
     .then(response => response.json())
     .then(data => {
       console.log(data);
+
+      document.body.onload = addElement;
+      function addElement() {
+      const locationName = document.createTextNode(data.name);
+      let location = document.getElementById("location");
+      location.appendChild(locationName);
+      }
+      addElement();
+
     })
     .catch(error => {
       console.error('Error:', error);
     });
+
 }
 
 function errorCallback(error) {
