f4f77c2ecf71aae942972cbc9f87f427ddb541b6
babtor <lucaswikstrom96@gmail.com>
2023-05-14 08:24:31 +0200

    fixed sizing on fetched icons

--- main.js
+++ main.js
@@ -31,24 +31,24 @@ function successCallback(position) {
 
       function addWeather() {
         let weatherIcons = {
-          '01d': 'https://openweathermap.org/img/wn/01d@2x.png',
-          '01n': 'https://openweathermap.org/img/wn/01n@2x.png',
-          '02d': 'https://openweathermap.org/img/wn/02d@2x.png',
-          '02n': 'https://openweathermap.org/img/wn/02nd@2x.png',
-          '03d': 'https://openweathermap.org/img/wn/03d@2x.png',
-          '03n': 'https://openweathermap.org/img/wn/03n@2x.png',
-          '04d': 'https://openweathermap.org/img/wn/04d@2x.png',
-          '04n': 'https://openweathermap.org/img/wn/04n@2x.png',
-          '09d': 'https://openweathermap.org/img/wn/09d@2x.png',
-          '09n': 'https://openweathermap.org/img/wn/09n@2x.png',
-          '10d': 'https://openweathermap.org/img/wn/10d@2x.png',
-          '10n': 'https://openweathermap.org/img/wn/10n@2x.png',
-          '11d': 'https://openweathermap.org/img/wn/11d@2x.png',
-          '11n': 'https://openweathermap.org/img/wn/11n@2x.png',
-          '13d': 'https://openweathermap.org/img/wn/13d@2x.png',
-          '13n': 'https://openweathermap.org/img/wn/13n@2x.png',
-          '50d': 'https://openweathermap.org/img/wn/50d@2x.png',
-          '50n': 'https://openweathermap.org/img/wn/50n@2x.png'
+          '01d': 'https://openweathermap.org/img/wn/01d@4x.png',
+          '01n': 'https://openweathermap.org/img/wn/01n@4x.png',
+          '02d': 'https://openweathermap.org/img/wn/02d@4x.png',
+          '02n': 'https://openweathermap.org/img/wn/02nd@4x.png',
+          '03d': 'https://openweathermap.org/img/wn/03d@4x.png',
+          '03n': 'https://openweathermap.org/img/wn/03n@4x.png',
+          '04d': 'https://openweathermap.org/img/wn/04d@4x.png',
+          '04n': 'https://openweathermap.org/img/wn/04n@4x.png',
+          '09d': 'https://openweathermap.org/img/wn/09d@4x.png',
+          '09n': 'https://openweathermap.org/img/wn/09n@4x.png',
+          '10d': 'https://openweathermap.org/img/wn/10d@4x.png',
+          '10n': 'https://openweathermap.org/img/wn/10n@4x.png',
+          '11d': 'https://openweathermap.org/img/wn/11d@4x.png',
+          '11n': 'https://openweathermap.org/img/wn/11n@4x.png',
+          '13d': 'https://openweathermap.org/img/wn/13d@4x.png',
+          '13n': 'https://openweathermap.org/img/wn/13n@4x.png',
+          '50d': 'https://openweathermap.org/img/wn/50d@4x.png',
+          '50n': 'https://openweathermap.org/img/wn/50n@4x.png'
         };
         let weatherCode = data.weather[0]["icon"];
         let weatherIcon = weatherIcons[weatherCode];
