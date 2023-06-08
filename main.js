ba19afe0cc054152f993e9fc515006fd3442aa96
babtor <lucaswikstrom96@gmail.com>
2023-06-08 12:38:32 +0200

    final fixes

--- main.js
+++ main.js
@@ -1,57 +1,56 @@
-let apiKey = 'e79a4558d37f0ddb584eb4a4854fb14b';
+let apiKey = "e79a4558d37f0ddb584eb4a4854fb14b";
 
 function fetchedListData() {
-fetch('city_list.json')
-.then(response => response.json())
-.then(data => {
-  createOptionsList(data);
+  fetch("city_list.json")
+    .then((response) => response.json())
+    .then((data) => {
+      createOptionsList(data);
 
-  function createOptionsList(data) {
-    let select = document.getElementById("cities");
-  
-    data.forEach((item) => {
-      let option = document.createElement("option");
-      option.value = [item.lat , item.lng];
-      option.textContent = item.city;
-      option.locationName = item.city;
-      select.appendChild(option);
+      function createOptionsList(data) {
+        let select = document.getElementById("cities");
+
+        data.forEach((item) => {
+          let option = document.createElement("option");
+          option.value = [item.lat, item.lng];
+          option.textContent = item.city;
+          option.locationName = item.city;
+          select.appendChild(option);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
-  }
-})
-  .catch(error => {
-    console.error('Error:', error);
-  });
 }
 
 function fetchedData() {
-    let selectedValue = document.getElementById("cities").value;
-    let latitude, longitude;
+  let selectedValue = document.getElementById("cities").value;
+  let latitude, longitude;
 
-      const selectedCoordinates = selectedValue.split(",");
-      const selectedLatitude = selectedCoordinates[0];
-      const selectedLongitude = selectedCoordinates[1];
-      latitude = selectedLatitude;
-      longitude = selectedLongitude;
-    
+  const selectedCoordinates = selectedValue.split(",");
+  const selectedLatitude = selectedCoordinates[0];
+  const selectedLongitude = selectedCoordinates[1];
+  latitude = selectedLatitude;
+  longitude = selectedLongitude;
 
-    let units = 'metric';
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        clearData();
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      clearData();
 
-        addWeather(data);
-        addTemperature(data);
-        addLocation();
-        timeDisplay();
-        setInterval(timeDisplay, 1000);
-      })
+      addWeather(data);
+      addTemperature(data);
+      addLocation();
+      timeDisplay();
+      setInterval(timeDisplay, 1000);
+    })
 
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function clearData() {
@@ -77,18 +76,22 @@ function clearData() {
 function addLocation() {
   let selectedOption = document.getElementById("cities").selectedOptions[0];
   let locationName = selectedOption.locationName;
-  let location =  document.getElementById("location");
+  let location = document.getElementById("location");
   location.textContent = locationName;
-  }
+}
 
 function addTemperature(data) {
   let temperature = data.main.temp;
-  let temperatureTextNode = document.createTextNode(Math.floor(temperature) + "°C");
+  let temperatureTextNode = document.createTextNode(
+    Math.floor(temperature) + "°C"
+  );
   let tempDiv = document.getElementById("temp");
-  tempDiv.appendChild(temperatureTextNode);  
+  tempDiv.appendChild(temperatureTextNode);
 
   let pressure = data.main.pressure;
-  let pressureTextNode = document.createTextNode("current pressure: " + pressure + " hPa");
+  let pressureTextNode = document.createTextNode(
+    "Current pressure: " + pressure + " hPa"
+  );
   let pressureDiv = document.getElementById("pres");
   pressureDiv.appendChild(pressureTextNode);
 
@@ -98,31 +101,31 @@ function addTemperature(data) {
   humidityDiv.appendChild(humidityTextNode);
 
   let wind = data.wind.speed;
-  let windTextNode = document.createTextNode("wind strength: " + wind + " M/S");
+  let windTextNode = document.createTextNode("Wind strength: " + wind + " M/S");
   let windDiv = document.getElementById("wind");
   windDiv.appendChild(windTextNode);
 }
 
 function addWeather(data) {
   let weatherIcons = {
-    '01d': 'https://openweathermap.org/img/wn/01d@4x.png',
-    '01n': 'https://openweathermap.org/img/wn/01n@4x.png',
-    '02d': 'https://openweathermap.org/img/wn/02d@4x.png',
-    '02n': 'https://openweathermap.org/img/wn/02nd@4x.png',
-    '03d': 'https://openweathermap.org/img/wn/03d@4x.png',
-    '03n': 'https://openweathermap.org/img/wn/03n@4x.png',
-    '04d': 'https://openweathermap.org/img/wn/04d@4x.png',
-    '04n': 'https://openweathermap.org/img/wn/04n@4x.png',
-    '09d': 'https://openweathermap.org/img/wn/09d@4x.png',
-    '09n': 'https://openweathermap.org/img/wn/09n@4x.png',
-    '10d': 'https://openweathermap.org/img/wn/10d@4x.png',
-    '10n': 'https://openweathermap.org/img/wn/10n@4x.png',
-    '11d': 'https://openweathermap.org/img/wn/11d@4x.png',
-    '11n': 'https://openweathermap.org/img/wn/11n@4x.png',
-    '13d': 'https://openweathermap.org/img/wn/13d@4x.png',
-    '13n': 'https://openweathermap.org/img/wn/13n@4x.png',
-    '50d': 'https://openweathermap.org/img/wn/50d@4x.png',
-    '50n': 'https://openweathermap.org/img/wn/50n@4x.png'
+    "01d": "https://openweathermap.org/img/wn/01d@4x.png",
+    "01n": "https://openweathermap.org/img/wn/01n@4x.png",
+    "02d": "https://openweathermap.org/img/wn/02d@4x.png",
+    "02n": "https://openweathermap.org/img/wn/02nd@4x.png",
+    "03d": "https://openweathermap.org/img/wn/03d@4x.png",
+    "03n": "https://openweathermap.org/img/wn/03n@4x.png",
+    "04d": "https://openweathermap.org/img/wn/04d@4x.png",
+    "04n": "https://openweathermap.org/img/wn/04n@4x.png",
+    "09d": "https://openweathermap.org/img/wn/09d@4x.png",
+    "09n": "https://openweathermap.org/img/wn/09n@4x.png",
+    "10d": "https://openweathermap.org/img/wn/10d@4x.png",
+    "10n": "https://openweathermap.org/img/wn/10n@4x.png",
+    "11d": "https://openweathermap.org/img/wn/11d@4x.png",
+    "11n": "https://openweathermap.org/img/wn/11n@4x.png",
+    "13d": "https://openweathermap.org/img/wn/13d@4x.png",
+    "13n": "https://openweathermap.org/img/wn/13n@4x.png",
+    "50d": "https://openweathermap.org/img/wn/50d@4x.png",
+    "50n": "https://openweathermap.org/img/wn/50n@4x.png",
   };
 
   let weatherCode = data.weather[0]["icon"];
@@ -136,7 +139,6 @@ function addWeather(data) {
   weatherDiv.appendChild(weatherTextNode);
 }
 
-
 function timeDisplay() {
   let today = new Date();
   let hours = today.getHours().toString().padStart(2, "0");
@@ -145,7 +147,6 @@ function timeDisplay() {
   let year = today.getFullYear().toString().padStart(2, "0");
   let month = today.getMonth().toString().padStart(2, "0");
   let day = today.getDate().toString().padStart(2, "0");
-  
 
   let currentDate = year + "-" + month + "-" + day;
   let time = hours + ":" + minutes + ":" + seconds;
@@ -159,4 +160,3 @@ fetchedListData();
 fetchedData();
 
 setInterval(fetchedData, 30 * 60 * 1000);
-
